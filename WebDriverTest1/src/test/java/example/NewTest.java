cc6aa0c46ba9bc481dd8f04e1bbf1c03b39f9623
39349 <39349@DESKTOP-1S89HDM.homenet.telecomitalia.it>
2022-09-28 12:32:21 +0200

    second commit

--- WebDriverTest1/src/test/java/example/NewTest.java
+++ WebDriverTest1/src/test/java/example/NewTest.java
@@ -12,6 +12,8 @@ import org.testng.annotations.Test;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.AfterTest;
 
+//second commit
+
 public class NewTest {
 	private WebDriver driver;
 	
